837fe02964831aaf946c85dd16373c830d98b3a0
Jesus <117941643+JesusV545@users.noreply.github.com>
2023-05-12 13:47:29 -0500

    search and save book functionality

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,108 +1,111 @@
-import React, { useState, useEffect } from 'react';
+import React from "react";
 import {
+  Jumbotron,
   Container,
+  CardColumns,
   Card,
   Button,
-  Row,
-  Col
-} from 'react-bootstrap';
+} from "react-bootstrap";
 
-import { getMe, deleteBook } from '../utils/API';
-import Auth from '../utils/auth';
-import { removeBookId } from '../utils/localStorage';
+import { useQuery, useMutation } from "@apollo/react-hooks";
+import { GET_ME } from "../utils/queries";
+import { REMOVE_BOOK } from "../utils/mutations";
+import Auth from "../utils/auth";
+import { removeBookId } from "../utils/localStorage";
 
 const SavedBooks = () => {
-  const [userData, setUserData] = useState({});
+  // useQuery hook to get current user's data
+  const { loading, data } = useQuery(GET_ME);
 
-  // use this to determine if `useEffect()` hook needs to run again
-  const userDataLength = Object.keys(userData).length;
+  // useMutation hook to remove a book from the user's saved books
+  const [removeBook] = useMutation(REMOVE_BOOK);
 
-  useEffect(() => {
-    const getUserData = async () => {
-      try {
-        const token = Auth.loggedIn() ? Auth.getToken() : null;
+  // extract the current user's data from the query results
+  const userData = data?.me || {};
 
-        if (!token) {
-          return false;
-        }
-
-        const response = await getMe(token);
-
-        if (!response.ok) {
-          throw new Error('something went wrong!');
-        }
-
-        const user = await response.json();
-        setUserData(user);
-      } catch (err) {
-        console.error(err);
-      }
-    };
-
-    getUserData();
-  }, [userDataLength]);
-
-  // create function that accepts the book's mongo _id value as param and deletes the book from the database
+  // function to handle deletion of a book from the user's saved books
   const handleDeleteBook = async (bookId) => {
+    // get the user's auth token
     const token = Auth.loggedIn() ? Auth.getToken() : null;
 
+    // return if there is no token
     if (!token) {
       return false;
     }
 
     try {
-      const response = await deleteBook(bookId, token);
+      // call the removeBook mutation with the book's ID
+      const { data } = await removeBook({
+        variables: { bookId },
+      });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const updatedUser = await response.json();
-      setUserData(updatedUser);
-      // upon success, remove book's id from localStorage
+      // upon success, remove the book's ID from localStorage
       removeBookId(bookId);
     } catch (err) {
       console.error(err);
     }
   };
 
-  // if data isn't here yet, say so
-  if (!userDataLength) {
+  // if the query is still loading, show a loading message
+  if (loading) {
     return <h2>LOADING...</h2>;
   }
 
   return (
     <>
-      <div fluid className="text-light bg-dark p-5">
+      {/* display a Jumbotron with the user's username */}
+      <Jumbotron fluid className="text-light bg-dark">
         <Container>
-          <h1>Viewing saved books!</h1>
+          <h1>Viewing {userData.username}'s saved books!</h1>
         </Container>
-      </div>
+      </Jumbotron>
+
+      {/* display the user's saved books */}
       <Container>
-        <h2 className='pt-5'>
-          {userData.savedBooks.length
-            ? `Viewing ${userData.savedBooks.length} saved ${userData.savedBooks.length === 1 ? 'book' : 'books'}:`
-            : 'You have no saved books!'}
+        <h2>
+          {userData.savedBooks?.length
+            ? `Viewing ${userData.savedBooks.length} saved ${
+                userData.savedBooks.length === 1 ? "book" : "books"
+              }:`
+            : "You have no saved books!"}
         </h2>
-        <Row>
-          {userData.savedBooks.map((book) => {
+
+        {/* display the saved books in a card layout */}
+        <CardColumns>
+          {userData.savedBooks?.map((book) => {
             return (
-              <Col md="4">
-                <Card key={book.bookId} border='dark'>
-                  {book.image ? <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' /> : null}
-                  <Card.Body>
-                    <Card.Title>{book.title}</Card.Title>
-                    <p className='small'>Authors: {book.authors}</p>
-                    <Card.Text>{book.description}</Card.Text>
-                    <Button className='btn-block btn-danger' onClick={() => handleDeleteBook(book.bookId)}>
-                      Delete this Book!
-                    </Button>
-                  </Card.Body>
-                </Card>
-              </Col>
+              <Card key={book.bookId} border="dark">
+                {/* if the book has an image, display it */}
+                {book.image ? (
+                  <Card.Img
+                    src={book.image}
+                    alt={`The cover for ${book.title}`}
+                    variant="top"
+                  />
+                ) : null}
+
+                <Card.Body>
+                  {/* display the book's title */}
+                  <Card.Title>{book.title}</Card.Title>
+
+                  {/* display the book's authors */}
+                  <p className="small">Authors: {book.authors}</p>
+
+                  {/* display the book's description */}
+                  <Card.Text>{book.description}</Card.Text>
+
+                  {/* display a button to delete the book */}
+                  <Button
+                    className="btn-block btn-danger"
+                    onClick={() => handleDeleteBook(book.bookId)}
+                  >
+                    Delete this Book!
+                  </Button>
+                </Card.Body>
+              </Card>
             );
           })}
-        </Row>
+        </CardColumns>
       </Container>
     </>
   );
